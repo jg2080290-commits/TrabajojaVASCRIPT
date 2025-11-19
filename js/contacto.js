4c62e9519c917d10bab22035fdeaec3afc5a11ee
jenifer santaella gonzalez <jg2080290@gmail.com>
2025-11-19 15:19:54 +0100

    Add map integration with custom markers; update styles and HTML structure

--- js/contacto.js
+++ js/contacto.js
@@ -1,9 +1,20 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+let map = L.map('map').setView([40.4506, -3.698603], 16);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    attribution: 'Mi OpenStreetMap'
 }).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-    .openPopup();
+let greenIcon = L.icon({
+    iconUrl: '../imagenes/leaf-green.png',
+    shadowUrl: '../imagenes/leaf-shadow.png',
+    iconSize: [38, 95],
+    shadowSize: [50, 64],
+    iconAnchor: [22, 95],
+    shadowAnchor: [4, 62],
+    popupAnchor: [-3, -76]
+});
+
+let marker = L.marker([40.4506, -3.698603], { icon: greenIcon })
+    .bindPopup('<a href="http://www.google.es">Estoy aquí</a>')
+    .openPopup()
+    .addTo(map);
